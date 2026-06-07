6aa38286a54c96f30b7f2ed6d7fc636b4bbde53e
Sheikh Haziq <sheikhhaziq76@gmail.com>
2026-06-07 17:41:36 +0530

    updated template

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ const localedir = GLib.build_filenamev([import.meta.datadir, "locale"])
 Gettext.bindtextdomain(import.meta.domain, localedir)
 Gettext.textdomain(import.meta.domain)
 
-const { App } = await import("../src/App")
+const { App } = await import("../src/app/application")
 const exitCode = await new App().runAsync([
   programInvocationName,
   ...programArgs,
